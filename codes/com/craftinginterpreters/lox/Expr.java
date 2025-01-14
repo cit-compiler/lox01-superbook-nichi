18e804334e86acd855c132cc3af10b89b8a63333
superbook-nichi <cien24001@g.nihon-u.ac.jp>
2025-01-14 11:06:59 +0900

    modify Expr.java visitor

--- codes/com/craftinginterpreters/lox/Expr.java
+++ codes/com/craftinginterpreters/lox/Expr.java
@@ -37,4 +37,26 @@ abstract class Expr {
     final Token operator;
     final Expr right;
   }
+
+  interface Visitor<R> {
+    R visitBinaryExpr(Binary expr);
+    // ...
+  }
+
+  static class Binary extends Expr {
+    Binary(Expr left, Token operator, Expr right) {
+      // ...
+    }
+
+    @Override
+    <R> R accept(Visitor<R> visitor) {
+      return visitor.visitBinaryExpr(this);
+    }
+
+    final Expr left;
+    // ...
+  }
+
+  abstract <R> R accept(Visitor<R> visitor);
+}
 }
