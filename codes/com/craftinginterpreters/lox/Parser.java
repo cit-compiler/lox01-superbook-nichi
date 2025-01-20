54045a2d4b732f230409176524b57eff1fdc704c
superbook-nichi <cien24001@g.nihon-u.ac.jp>
2025-01-21 00:07:52 +0900

    modify Parser.java

--- codes/com/craftinginterpreters/lox/Parser.java
+++ codes/com/craftinginterpreters/lox/Parser.java
@@ -5,6 +5,8 @@ import java.util.List;
 import static com.craftinginterpreters.lox.TokenType.*;
 
 class Parser {
+  private static class ParseError extends RuntimeException {}
+
   private final List<Token> tokens;
   private int current = 0;
 
@@ -121,15 +123,17 @@ class Parser {
       return new Expr.Grouping(expr);
     }
 
-    throw new ParseError("Expect expression.");
+    throw error(peek(), "Expect expression.");
   }
 
-  private void consume(TokenType type, String message) {
-    if (check(type)) {
-      advance();
-      return;
-    }
-    throw new ParseError(message);
+  private Token consume(TokenType type, String message) {
+    if (check(type)) return advance();
+
+    throw error(peek(), message);
+  }
+
+  private ParseError error(Token token, String message) {
+    Lox.error(token, message);
+    return new ParseError();
   }
 }
-
